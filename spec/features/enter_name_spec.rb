5842484645ed5f324a6efade3b622afe50ff0704
James Gardiner <jamesgardiner075@gmail.com>
2016-10-12 18:03:51 +0100

    Modify CSS

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -3,6 +3,6 @@ require "./app"
 feature "Enter name" do
   scenario "User enters name" do
     sign_in_and_play
-    expect(page).to have_text("Jimmy vs Bob")
+    expect(page).to have_text("Jimmy HITPOINTS = 100 vs Bob HITPOINTS = 100")
   end
 end
