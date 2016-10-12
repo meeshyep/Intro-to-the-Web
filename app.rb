4e28f4a12613ce60e0291a6ecc51d10da8e4aee8
ManuCiao <mn.sabatino@gmail.com>
2016-10-12 15:58:29 +0100

    Add Post/Redirect

--- app.rb
+++ app.rb
@@ -1,14 +1,24 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
+enable :sessions
+
   get '/' do
     'Hello Battle!'
     erb(:index)
   end
 
   post '/names' do
-    @name_1 = params[:name_1]
-    @name_2 = params[:name_2]
+    session[:name_1] = params[:name_1]
+    session[:name_2] = params[:name_2]
+    redirect '/play'
+
+  end
+
+  get '/play' do
+    @name_1 = session[:name_1]
+    @name_2 = session[:name_2]
     puts params
     erb (:play)
   end
